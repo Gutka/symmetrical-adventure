dd7024dbdb4c0f0d3cad4798e60fb3be3d07b639
Gut Michal <eMichalGut@gmail.com>
2019-04-14 20:07:16 +0200

    0.02 (Movement)

--- symmetricalAdventure/Engine/ViewModels/GameSession.cs
+++ symmetricalAdventure/Engine/ViewModels/GameSession.cs
@@ -5,14 +5,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using Engine.Factories;
 
 namespace Engine.ViewModels
 {
-    public class GameSession
+    public class GameSession : INotifyPropertyChanged
     {
+        private Location _CurrentLocation;
+
         public World CurrentWorld { get; set; }
         public Player CurrentPlayer { get; set; }
-        public Location CurrentLocation { get; set; }
+        public Location CurrentLocation
+        {
+            get
+            {
+                return _CurrentLocation;
+            }
+            set
+            {
+                _CurrentLocation = value;
+                OnPropertyChanged("CurrentLocation");
+                OnPropertyChanged("HasLocationToNorth");
+                OnPropertyChanged("HasLocationToEast");
+                OnPropertyChanged("HasLocationToSouth");
+                OnPropertyChanged("HasLocationToWest");
+            }
+        }
+        public bool HasLocationToNorth
+        {
+            get
+            {
+                return CurrentWorld.AtLocation(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1) != null;
+            }
+        }
+        public bool HasLocationToEast
+        {
+            get
+            {
+                return CurrentWorld.AtLocation(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate) != null;
+            }
+        }
+        public bool HasLocationToSouth
+        {
+            get
+            {
+                return CurrentWorld.AtLocation(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1) != null;
+            }
+        }
+        public bool HasLocationToWest
+        {
+            get
+            {
+                return CurrentWorld.AtLocation(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate) != null;
+            }
+        }
 
         public GameSession()
         {
@@ -29,6 +76,30 @@ namespace Engine.ViewModels
 
             CurrentLocation = CurrentWorld.AtLocation(0, 0);
 
+        }
+
+        public void MoveNorth()
+        {
+            CurrentLocation = CurrentWorld.AtLocation(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1);
+        }
+        public void MoveSouth()
+        {
+            CurrentLocation = CurrentWorld.AtLocation(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1);
+        }
+        public void MoveWest()
+        {
+            CurrentLocation = CurrentWorld.AtLocation(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate);
+        }
+        public void MoveEast()
+        {
+            CurrentLocation = CurrentWorld.AtLocation(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
